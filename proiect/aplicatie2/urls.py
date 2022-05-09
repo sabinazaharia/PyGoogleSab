864a6d16bdad79a8ffb52f6cc89ce83e950180e4
Sabina Zaharia <sbnzaharia@gmail.com>
2022-05-09 12:50:07 +0300

    Modificare, stergere si activare

--- proiect/aplicatie2/urls.py
+++ proiect/aplicatie2/urls.py
@@ -7,4 +7,8 @@ app_name = 'companies'
 urlpatterns = [
     path('', views.CompaniesView.as_view(), name='lista_companii'),
     path('adaugare/', views.CreateCompaniesView.as_view(), name='adauga'),
+    #pentru ca modifica o data deja existenta, folosim un primarykey
+    path('<int:pk>/update/', views.UpdateCompaniesView.as_view(), name='modifica'),
+    path('<int:pk>/delete/', views.delete_companie, name='sterge'),
+    path('<int:pk>/activate/', views.activate_companie, name='activeaza'),
 ]
--- proiect/aplicatie2/views.py
+++ proiect/aplicatie2/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.urls import reverse
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from aplicatie2.models import Companies
 
@@ -17,3 +19,23 @@ class CreateCompaniesView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse('companies:lista_companii')
+
+
+class UpdateCompaniesView(LoginRequiredMixin, UpdateView):
+    model = Companies
+    fields = ['nume', 'website', 'tip_companie']
+    template_name = 'aplicatie2/companies_form.html'
+
+    def get_success_url(self):
+        return reverse('companies:lista_companii')
+
+
+@login_required
+def delete_companie(request, pk):
+    Companies.objects.filter(id=pk).update(activ=0)
+    return redirect('companies:lista_companii')\
+
+@login_required
+def activate_companie(request, pk):
+    Companies.objects.filter(id=pk).update(activ=1)
+    return redirect('companies:lista_companii')
